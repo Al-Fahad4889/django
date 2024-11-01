316eb8e9c94db59e2e8d10db0c44900b4af757b1
Al-Fahad4889 <alfahad4889@gmail.com>
2024-11-01 15:47:13 +0600

    main branch

--- mid/shop/models.py
+++ mid/shop/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     name = models.CharField(max_length=100) 
     description = models.TextField(null=True, blank=True) 
     publication_date = models.DateField(null=True, blank=True) 
-    price = models.DecimalField(default=0, max_digits=10, decimal_places=2) 
+    price = models.DecimalField(default=0, max_digits=10, decimal_places=3) 
      
